9f13f6f20d863fee94764f3f4e4c726973c56610
Алиса <petrunina.alisa@gmail.com>
2022-07-04 23:22:36 +0300

    массивы

--- Program.cs
+++ Program.cs
@@ -36,18 +36,52 @@
 //Console.WriteLine(N + "->" + sum_numbers(N));
 //}
 
-void sum_numbers(int num)
-{
-    int sum = 0;
-    for ( int i = 1; i <= num; i++)
-    {
-    sum = sum + i;
-    }
-    Console.WriteLine(num + "->" + sum);
-}
-    for (int i = 1; i <= 5; i++)
-{
-Console.WriteLine ("Input N");
-int N = Convert.ToInt32(Console.ReadLine());
-sum_numbers(N);
-}
+//void sum_numbers(int num)
+//{
+ //   int sum = 0;
+ //   for ( int i = 1; i <= num; i++)
+ //   {
+ //   sum = sum + i;
+ //   }
+ //   Console.WriteLine(num + "->" + sum);
+//}
+//    for (int i = 1; i <= 5; i++)
+//{
+//Console.WriteLine ("Input N");
+//int N = Convert.ToInt32(Console.ReadLine());
+//sum_numbers(N);
+//}
+
+//создание случайного массива через инт
+//int[] CreateArray()
+//{
+//    Console.WriteLine("Введите количество элементов массива");
+//    int size = Convert.ToInt32(Console.ReadLine());
+//    int[] RandomArray = new int[size];
+//    for (int i = 0; i < size; i++)
+//    {   
+ //       Console.WriteLine($"Введите {i+1} элемент массива");
+//        RandomArray[i] = Convert.ToInt32(Console.ReadLine());
+//void ShowArray(int[] array)
+//{
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        Console.Write(array[i] + " ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.Clear();
+Console.WriteLine("Введите количество элементов в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myRandomArray = CreateRandomArray(num, min, max);
+ShowArray(myRandomArray);
+Console.WriteLine("-------");
+int[] myArray = CreateArray();
+ShowArray(myArray);
+
+
