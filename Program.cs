2e4ed90550a8e451d55c1a8ee3453489e5a1dee2
Алиса <petrunina.alisa@gmail.com>
2022-07-04 21:15:15 +0300

    Sem2c#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Console.WriteLine("Ввод числа N");
+//int N = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//for ( int i = 1; i <= N; i++)
+//{
+//    sum = sum + i;
+//}
+//Console.WriteLine(sum);
+
+//c while
+//Console.WriteLine("Ввод числа N");
+//int N = Convert.ToInt32(Console.ReadLine());
+//int sum = 1;
+//int i = 0;
+//while (i <= N)
+//{
+//    sum = sum + i;
+//    i+=1;
+//}
+ //   Console.WriteLine(sum);
+
+//с использованием метода
+int sum_numbers(int num)
+{
+    int sum = 0;
+    for ( int i = 1; i <= num; i++)
+    {
+    sum = sum + i;
+    }
+    return sum;
+}
+    for ( int i = 1; i <= 5; i++)
+{
+Console.WriteLine ("Input N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(N + "->" + sum_numbers(N));
+}
