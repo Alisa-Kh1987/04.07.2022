9c00d082df24f9c716c7cd961a75efde00c02402
Алиса <petrunina.alisa@gmail.com>
2022-07-08 00:07:50 +0300

    массив

--- Program.cs
+++ Program.cs
@@ -52,36 +52,38 @@
 //sum_numbers(N);
 //}
 
-//создание случайного массива через инт
-//int[] CreateArray()
-//{
-//    Console.WriteLine("Введите количество элементов массива");
-//    int size = Convert.ToInt32(Console.ReadLine());
-//    int[] RandomArray = new int[size];
-//    for (int i = 0; i < size; i++)
-//    {   
- //       Console.WriteLine($"Введите {i+1} элемент массива");
-//        RandomArray[i] = Convert.ToInt32(Console.ReadLine());
-//void ShowArray(int[] array)
-//{
-//    for (int i = 0; i < array.Length; i++)
-//    {
-//        Console.Write(array[i] + " ");
-//    }
-//    Console.WriteLine();
-//}
-//Console.Clear();
+//создание случайного массива
+int[] CreateRandomArray(int N, int start, int end) //N - количество элементов, старт и энд - минимум и максимум массива
+{
+    int [] RandomArray = new int [N];
+        for (int i = 0; i < N; i++)
+        {   
+            RandomArray [i] = new Random().Next(start, end + 1);
+        }
+        return RandomArray;
+}
+//вывод на экран массива
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();  //чтобы автоматически при запуске программы консоль очищалась
+///основная программа
 Console.WriteLine("Введите количество элементов в массиве");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите первое число случайно генерируемого диапазона");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
 int max = Convert.ToInt32(Console.ReadLine());
-
+    //сохраняем в новый массив
 int[] myRandomArray = CreateRandomArray(num, min, max);
+//сюда отправляем, чтобы вывести на экран
 ShowArray(myRandomArray);
-Console.WriteLine("-------");
-int[] myArray = CreateArray();
-ShowArray(myArray);
+
+
 
 
